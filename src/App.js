3665515e44dd42c9dd24b5a0caff470cac2a6b36
Antraxmin <antraxmin@naver.com>
2022-10-10 13:55:24 +0900

    demo version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [applist, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [like, setlike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{ applist[0] } <span onClick={ () => {setlike(like+1)} }>👍</span> {like} </h4>
+        <p>10월 10일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ applist[1] }</h4>
+        <p>10월 10일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ applist[2] }</h4>
+        <p>10월 10일 발행</p>
+      </div>
     </div>
   );
 }
