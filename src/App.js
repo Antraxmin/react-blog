49c67bfb81a9161c07d0aa666f95fcac2a60bcba
Antraxmin <antraxmin@naver.com>
2022-10-17 00:06:10 +0900

    Initialize code for study

--- src/App.js
+++ src/App.js
@@ -1,81 +1,55 @@
-/* eslint-disable */
-import logo from './logo.svg';
-import './App.css';
+/*eslint-disable*/
 import { useState } from 'react';
+import './App.css'
 
-function App() {
-  let [applist, setApplist] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [like, setlike] = useState(0);
-  let [modal, setModal] = useState(false);
-  let [title, setTitle] = useState(0);   // 현재 title UI를 저장할 state
+function App(){
+ 
+  const [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  const [따봉, 따봉변경] = useState(0);
+  const [modal, setModal] = useState(false);
+
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>ReactBlog</h4>
+        <div>개발 blog</div>
       </div>
-      <button onClick={() => {
-        let list_copy = [...applist];
-        list_copy.sort();
-        setApplist(list_copy);
-      }}
-      >정렬</button>
-      <button onClick={ ()=>{
-        let copy = [...applist];
+      <button onClick={()=>{
+        let copy = [...글제목];
         copy[0] = '여자코트 추천';
-        setApplist(copy);
-      } }>글 수정하기</button>
-
-      <div>
-        {
-          // applist 요소 개수만큼 function(a, i)을 반복하여 호출한다. 
-          applist.map(function(a, i) {
-            return (
-              <div className="list" key={i}>
-                <h4 onClick={ ()=>{ setModal(!modal); setTitle(i);}}> {applist[i]} 
-                  <span onClick={ () => { setlike(like+1)} }>👍</span>
-                  {like}</h4>
-                <p>2월 18일 발행</p>
-              </div>
-            )
-          })
-        }
-      </div>
-{/* 
+        글제목변경(copy);
+        }}>버튼</button>
+        <button onClick={()=>{
+        let copy = [...글제목].sort();
+        글제목변경(copy);
+        }}>정렬</button>
       <div className="list">
-        <h4>{ applist[0] } <span onClick={ () => {setlike(like+1)} }>👍</span> {like} </h4>
-        <p>10월 10일 발행</p>
+        <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ applist[1] }</h4>
-        <p>10월 10일 발행</p>
+        <h4>{ 글제목[1] } <span>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4 onClick={()=>{setModal(!modal)}}>{ applist[2] }</h4>
-        <p>10월 10일 발행</p>
-      </div> */}
+        <h4>{ 글제목[2] } <span>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+      </div>
       {
-       // 부모 컴포넌트인 App()에서 자식 컴포넌트인 Modal()로 state 전송
-        modal == true ? <Modal 
-        title={title} 
-        color={'#EDE4E0'} 
-        setApplist={setApplist} 
-        applist={applist}/> 
-        : null
+        modal == true ? <Modal></Modal> : null
       }
     </div>
-  );
+  )
 }
 
-// 컴포넌트 
-function Modal(props) {
+function Modal() {
   return (
-    <div className="modal" style={{background : props.color}}>
-      <h4>{props.applist[props.title]}</h4>
+    <div className="modal">
+      <h4>제목</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button>글수정</button>
-    </div> 
+    </div>
   )
 }
 
